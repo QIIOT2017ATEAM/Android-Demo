4348932d89d39dbc4e5b6b1c69715342cb7e4b28
Unknown <gksjkg@gmail.com>
2017-07-18 16:38:30 -0700

    17.7.18
    fragement 4page make

---
+++ app/src/main/java/com/example/sec/myapplication/Login.java
@@ -0,0 +1,8 @@
+package com.example.sec.myapplication;
+
+/**
+ * Created by sec on 2017-07-18.
+ */
+
+public class Login {
+}
---
+++ app/src/main/java/com/example/sec/myapplication/MainActivity.java
@@ -0,0 +1,105 @@
+package com.example.sec.myapplication;
+
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private final int FRAGMENT1 = 1;
+    private final int FRAGMENT2 = 2;
+    private final int FRAGMENT3 = 3;
+    private final int FRAGMENT4 = 4;
+
+    private Button btn_1, btn_2, btn_3, btn_4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // 위젯에 대한 참조
+        btn_1 = (Button)findViewById(R.id.btn_1);
+        btn_2 = (Button)findViewById(R.id.btn_2);
+        btn_3 = (Button)findViewById(R.id.btn_3);
+        btn_4 = (Button)findViewById(R.id.btn_4);
+
+        // 탭 버튼에 대한 리스너 연결
+        btn_1.setOnClickListener(this);
+        btn_2.setOnClickListener(this);
+        btn_3.setOnClickListener(this);
+        btn_4.setOnClickListener(this);
+
+        // 임의로 액티비티 호출 시점에 어느 프레그먼트를 프레임레이아웃에 띄울 것인지를 정함
+
+        callFragment(FRAGMENT1);
+
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_1 :
+                // '버튼1' 클릭 시 '프래그먼트1' 호출
+                callFragment(FRAGMENT1);
+                break;
+
+            case R.id.btn_2 :
+                // '버튼2' 클릭 시 '프래그먼트2' 호출
+                callFragment(FRAGMENT2);
+                break;
+
+            case R.id.btn_3 :
+                // '버튼2' 클릭 시 '프래그먼트3' 호출
+                callFragment(FRAGMENT3);
+                break;
+            case R.id.btn_4 :
+                // '버튼2' 클릭 시 '프래그먼트3' 호출
+                callFragment(FRAGMENT4);
+                break;
+        }
+    }
+
+    private void callFragment(int frament_no){
+
+        // 프래그먼트 사용을 위해
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        switch (frament_no){
+            case 1:
+                // '프래그먼트1' 호출
+                Fragment1 fragment1 = new Fragment1();
+                transaction.replace(R.id.fragment_container, fragment1);
+                transaction.commit();
+                break;
+
+            case 2:
+                // '프래그먼트2' 호출
+                Fragment2 fragment2 = new Fragment2();
+                transaction.replace(R.id.fragment_container, fragment2);
+                transaction.commit();
+                break;
+
+            case 3:
+                // '프래그먼트3' 호출
+                Fragment3 fragment3 = new Fragment3();
+                transaction.replace(R.id.fragment_container, fragment3);
+                transaction.commit();
+                break;
+
+            case 4:
+                // '프래그먼트4' 호출
+                Fragment4 fragment4 = new Fragment4();
+                transaction.replace(R.id.fragment_container, fragment4);
+                transaction.commit();
+                break;
+        }
+
+    }
+
+
+}
