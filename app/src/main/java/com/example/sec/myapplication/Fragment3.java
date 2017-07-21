15216f29234612fcb9e8391eb922d0b89b9db570
sangeun <ksg95c@gmail.com>
2017-07-21 15:22:32 -0700

    170721

--- app/src/main/java/com/example/sec/myapplication/Fragment3.java
+++ app/src/main/java/com/example/sec/myapplication/Fragment3.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+
 /**
  * A simple {@link Fragment} subclass.
  */
@@ -20,7 +21,6 @@ public class Fragment3 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment3, container, false);
     }
