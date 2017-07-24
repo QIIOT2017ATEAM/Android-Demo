fdc15411fa2e75bd3cdd2eb53be6505ba7c92428
sangeun <ksg95c@gmail.com>
2017-07-24 09:24:14 -0700

    17/07/24

--- app/src/main/java/com/example/sec/myapplication/Find.java
+++ app/src/main/java/com/example/sec/myapplication/Find.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TabHost;
 
 public class Find extends AppCompatActivity {
@@ -20,11 +22,16 @@ public class Find extends AppCompatActivity {
         tabHost1.setup();
 
         TabHost.TabSpec ts1 = tabHost1.newTabSpec("Tab Spec 1") ;
-        ts1.setContent(R.id.ID) ;
-        ts1.setIndicator("ID") ;
-        tabHost1.addTab(ts1)  ;
+        ts1.setContent(R.id.ID);
+        ts1.setIndicator("ID");
+        tabHost1.addTab(ts1);
 
-       TabHost.TabSpec ts2 = tabHost1.newTabSpec("Tab Spec 2") ;
+        Spinner question = (Spinner) findViewById(R.id.txt_question_type);
+        ArrayAdapter questionA = ArrayAdapter.createFromResource(this, R.array.question, android.R.layout.simple_spinner_item);
+        questionA.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        question.setAdapter(questionA);
+
+        TabHost.TabSpec ts2 = tabHost1.newTabSpec("Tab Spec 2") ;
         ts2.setContent(R.id.PASSWORD) ;
         ts2.setIndicator("PASSWORD") ;
         tabHost1.addTab(ts2) ;
@@ -46,6 +53,5 @@ public class Find extends AppCompatActivity {
                 startActivity(pwfind); // 얘가 액티비티 실행시키는거 있어야함꼭
             }
         });
-
     }
 }
