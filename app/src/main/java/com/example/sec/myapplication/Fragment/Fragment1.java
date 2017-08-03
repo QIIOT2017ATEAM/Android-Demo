a2c95b033aaee07c17623410c4381a17f68a8782
sangeun <ksg95c@gmail.com>
2017-08-03 14:16:16 -0700

    17.08.03

--- app/src/main/java/com/example/sec/myapplication/Fragment/Fragment1.java
+++ app/src/main/java/com/example/sec/myapplication/Fragment/Fragment1.java
@@ -3,6 +3,7 @@ package com.example.sec.myapplication.Fragment;
 import android.content.Context;
 import android.graphics.Color;
 import android.graphics.Movie;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 //import android.support.v4.app.Fragment;
 import android.app.Fragment;
@@ -10,6 +11,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -24,16 +27,19 @@ import com.github.mikephil.charting.data.LineDataSet;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.lang.String;
 
+import static android.R.attr.id;
+import static android.R.attr.text;
+
 /**
  * A simple {@link Fragment} subclass.
  */
 public class Fragment1 extends Fragment {
-
     public View view;
     TextView text_input;
     int Hcount = 0;
@@ -60,6 +66,21 @@ public class Fragment1 extends Fragment {
             Log.e("test", "" + text_input);
         //setData();
     }
+    /*
+    public void maxHeart(int hr) { //심박수 넣어줌 여기다가
+        Hcount++;
+        if ( (max_val != null) || (Integer.parseInt(max_val.getText) < hr) )
+            max_val.setText("" + hr); //인트타입 잡아넣는법
+        //setData();
+    }*/
+
+    /*
+    public void minHeart(int hr) { //심박수 넣어줌 여기다가
+        Hcount++;
+        if ( (min_val != null) || (Integer.parseInt(min_val.getText) > hr) )
+            min_val.setText("" + hr); //인트타입 잡아넣는법
+        //setData();
+    }*/
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -67,17 +88,22 @@ public class Fragment1 extends Fragment {
         view = inflater.inflate(R.layout.fragment1,container, false);  //xml 에 씌여져 있는 view 의 정의를 실제 view 객체로 만드는 역할을
                                                                        //LayoutInfalter 랑 이거 차이가 뭔가용??
 
-         text_input = (TextView) view.findViewById(R.id.text_input);
+        text_input = (TextView) view.findViewById(R.id.text_input);
 
         //text_input.setText(getNumber().toString());
         // text_input.setText(readMessage.toString());
         //MainActivity mainActivity = new MainActivity();  //mainactivity load
         // Inflate the layout for this fragment
 
-        ;
-
+        //startRocketTweenAnimation();
         return view;
     }
+    /*
+    private void startRocketTweenAnimation(){
+        Animation heart_anim = AnimationUtils.loadAnimation(this.getActivity(), R.id.scale);
+        text_input.startAnimation(heart_anim);
+    }
+    */
 
     public void setData() {
         Hcount++;
@@ -102,8 +128,5 @@ public class Fragment1 extends Fragment {
         Hchart.setData(heart_data);
         Hchart.invalidate(); //  dont forget to refresh the drawing
     }
-
-
-
 
 }
--- app/src/main/java/com/example/sec/myapplication/RegisterID.java
+++ app/src/main/java/com/example/sec/myapplication/RegisterID.java
@@ -1,16 +1,39 @@
 package com.example.sec.myapplication;
 
+
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.os.AsyncTask;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 
 public class RegisterID extends AppCompatActivity {
+
+
+    public  EditText input_User_ID, input_User_Password, input_first_name,input_last_name,input_User_Birthday;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,16 +44,56 @@ public class RegisterID extends AppCompatActivity {
         getSupportActionBar().setTitle("Register ID");//액션바 타이틀 변경하는거
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#ffd777"))); //액션바 배경색 변경 0xFF339999
 
+        input_User_ID = (EditText)findViewById(R.id.input_User_ID);
+        input_User_Password = (EditText)findViewById(R.id.input_User_Password);
+        input_first_name = (EditText)findViewById(R.id.input_first_name);
+        input_last_name = (EditText)findViewById(R.id.input_last_name);
+        input_User_Birthday = (EditText)findViewById(R.id.input_User_Birthday);
+
         Button submit = (Button)findViewById(R.id.btn_submit);
 
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String User_ID = input_User_ID.getText().toString(); //toString으로 문자변환
+                String User_Password = input_User_Password.getText().toString(); //toString으로 문자변환
+                String first_name = input_first_name.getText().toString(); //toString으로 문자변환
+                String last_name = input_last_name.getText().toString(); //toString으로 문자변환
+                String User_Birthday = input_User_Birthday.getText().toString(); //toString으로 문자변환
+                //Integer.parseInt(); 문자열을 숫자로
+
+                JsonTransfer userdata_transfer = new JsonTransfer();
+
+                try
+                {
+                    JSONObject json_dataTransfer = new JSONObject();  //JSONObject는 JSON을 만들기 위함.
+                    //json_dataTransfer에 ("키값" : "보낼데이터") 형식으로 저장한다.
+                    json_dataTransfer.put("user_id", input_User_ID.getText().toString());
+                    json_dataTransfer.put("user_password", input_User_Password.getText().toString());
+                    json_dataTransfer.put("first_name", input_first_name.getText().toString());
+                    json_dataTransfer.put("last_name", input_last_name.getText().toString());
+                    json_dataTransfer.put("user_birthday", input_User_Birthday.getText().toString());
+                    //json_dataTransfer의 데이터들을 하나의 json_string으로 묶는다.
+                    String json_string = json_dataTransfer.toString();
+
+
+                    //보내기 전에 json_string 양 쪽 끝에 대괄호를 붙인다. (Object로 처리하기 때문이다. 만약 Array로 처리한다면, 대괄호는 필요없다고 한다.)
+                    userdata_transfer.execute("http://teama-iot.calit2.net/slim-api/receive-user-data","["+json_string+"]");  //보낼주소추가
+
+                    //Toast.makeText(getApplicationContext(), json_string, Toast.LENGTH_LONG);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
                 Intent RegisterID = new Intent(RegisterID.this, Login.class);
                 startActivity(RegisterID);
                 finish();
             }
         });
-
     }
+
+
+
+
 }
